db6c00483ca7b26a8c921861f95defd300d37a0b
FeRan147 <feran@uvaga.by>
2014-12-18 23:45:17 +0300

    Update

---
+++ BinTree/BinTree.h
@@ -0,0 +1,29 @@
+#pragma once
+
+typedef int Data;
+
+class BinTree
+{
+public:
+	BinTree(); //Конструктор
+	BinTree(const BinTree &original); //Конструктор копирования
+	BinTree &operator=(const BinTree &rhs);  //Оператор присваивания
+	BinTree(BinTree &&victim); //Конструктор перемещения
+	BinTree &operator=(BinTree &&rhs); //Оператор переместительного присваивания
+	~BinTree(); //Деструктор
+
+	bool find(Data data) const;
+	void push(Data data);
+	void pop(Data data);
+	void output() const;
+private:
+	struct Node
+	{
+		Data data;
+		Node *right;
+		Node *left;
+		Node(Data data);
+		~Node();
+	} *root;
+	static void output(Node *node);
+};
