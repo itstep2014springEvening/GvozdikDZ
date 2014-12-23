d2861a45ccc219ae553590dc0d3e3b94a732ecc9
FeRan147 <feran@uvaga.by>
2014-12-23 23:19:00 +0300

    StackPimplNew

---
+++ StackPimplNew/prettystack.h
@@ -0,0 +1,14 @@
+#ifndef PRETTYSTACK_H
+#define PRETTYSTACK_H
+
+#include "consolestack.h"
+
+class PrettyStack : public ConsoleStack
+{
+public:
+    //Great VI
+private:
+    void outputImplementaion()const override;
+};
+
+#endif // PRETTYSTACK_H
---
+++ StackPimplNew/stack.h
@@ -0,0 +1,33 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef int Data;
+
+class Stack
+{
+public:
+    Stack(); //Конструктор
+    Stack(const Stack &original); //Конструктор копирования
+    Stack &operator=(const Stack &rhs);  //Оператор присваивания
+    Stack(Stack &&victim); //Конструктор перемещения
+    Stack &operator=(Stack &&rhs); //Оператор переместительного присваивания
+    ~Stack(); //Деструктор
+
+    void push(Data data);
+    void pop();
+    Data onTop() const;
+    bool isEmpty() const;
+    void clear();
+
+protected:
+
+    void getArrayRepresentation(Data *&array, int &size)const;
+    bool getNext(Data &data, bool firstTime = false)const;
+    void acceptVisitor(void (*visitor)(Data data))const;
+
+private:
+    class Implementation;
+    Implementation *pimpl;
+};
+
+#endif // STACK_H
