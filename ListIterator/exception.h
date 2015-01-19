0ce6b5a346ef036601c4e1e5d5b694a5bc746565
FeRan147 <feran@uvaga.by>
2015-01-19 21:01:35 +0300

    Change

--- ListIterator/exception.h
+++ ListIterator/exception.h
@@ -1,12 +1,17 @@
 #ifndef EXCEPTION_H
 #define EXCEPTION_H
 
-
+namespace std
+{
 class exception
 {
 public:
-    exception();
-    ~exception();
-};
+    exception() noexcept;
+    exception(const exception&) noexcept;
+    exception &operator=(const exception&) noexcept;
+    virtual ~exception() noexcept;
 
+    virtual const char *what() const noexcept;
+};
+}
 #endif // EXCEPTION_H
